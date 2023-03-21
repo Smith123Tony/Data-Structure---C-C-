afaa637c36b4cbe952eed5da4fe3a56207277a84
Smith123Tony <JK.Tony@outlook.com>
2023-03-21 10:38:41 +0800

    数组栈的基本操作。

---
+++ 栈和队列/StackQueue/test.c
@@ -0,0 +1,55 @@
+//数组栈的实现
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include"Stack.h"
+
+void TestStack1()
+{
+	ST st;
+	StackInit(&st);
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+
+	StackPop(&st);
+	StackPop(&st);
+	StackPop(&st);
+	StackPop(&st);
+	StackPop(&st);
+	//printf("%d\n", StackTop(&st));
+	//StackDestroy(&st);
+}
+
+void TestStack2()
+{
+	ST st;
+	StackInit(&st);
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+
+	printf("%d ", StackTop(&st));
+	StackPop(&st);
+	printf("%d ", StackTop(&st));
+	StackPop(&st);
+
+	StackPush(&st, 5);
+	StackPush(&st, 6);
+
+	while (!StackEmpty(&st))
+	{
+		printf("%d ", StackTop(&st));
+		StackPop(&st);
+	}
+
+	StackDestroy(&st);
+}
+
+int main()
+{
+	//TestStack1();
+	TestStack2();
+	return 0;
+}
